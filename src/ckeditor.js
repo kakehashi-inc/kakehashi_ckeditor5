1783c633156219455f46c6551bc9229d8641ea52
jp-yendo <easymaster2010@gmail.com>
2022-12-19 18:54:21 +0900

    h6まで拡張

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -119,6 +119,7 @@ Editor.builtinPlugins = [
 
 // Editor configuration.
 Editor.defaultConfig = {
+    language: 'ja',
     fontFamily: {
         options: [
             'default',
@@ -174,10 +175,20 @@ Editor.defaultConfig = {
         shouldNotGroupWhenFull: true,
     },
     blockToolbar: {
-        items: ['paragraph', '|', 'heading1', 'heading2', 'heading3', '|', 'blockQuote'],
+        items: ['paragraph', '|', 'heading1', 'heading2', 'heading3', 'heading4', 'heading5', 'heading6', '|', 'blockQuote'],
         shouldNotGroupWhenFull: true,
     },
-    language: 'ja',
+    heading: {
+        options: [
+            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+            { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+            { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+            { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+            { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+        ]
+    },
     image: {
         toolbar: [
             'imageStyle:inline',
